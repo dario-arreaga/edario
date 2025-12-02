6bd9536b8286b0479f034c7eb97f900507eb5b93
Alexis Arreaga <darioarreaga14@gmail.com>
2025-12-02 11:08:23 -0500

    Agregado código de tracking Umami

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="2d2899e7-348c-419c-b75d-04bfd898f30f"></script>
+      </head>
       <body>
         {children}
       </body>
